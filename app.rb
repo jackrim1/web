17376b9059f2ad93d3bbda2d618536b6b7ad3e82
Jackrim1 <jriminton@gmail.com>
2016-04-25 17:39:05 +0100

    The cat is cleverer now

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 
 require 'sinatra'
 set :session_secret, 'super secret'
+
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
+
 
 
