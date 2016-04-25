f0ce44f7c62020101f32997aac1ed8468daaf1c9
Jackrim1 <jriminton@gmail.com>
2016-04-25 17:18:20 +0100

    cat

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
+
 require 'sinatra'
-
-get '/' do
-  'hello world!'
+set :session_secret, 'super secret'
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
 
-get '/secret' do
-  'shh this is secret'
-end
+
